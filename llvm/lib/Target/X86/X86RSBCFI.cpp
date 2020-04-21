0689e56c6444f1c003df0a84010a99fb1ba0813f
Pinghai Yuan <pinghaiyuan@gmail.com>
2020-04-21 09:37:00 +0800

    Try to do instrumentation  Changes to be committed: 	modified:   X86RSBCFI.cpp

--- llvm/lib/Target/X86/X86RSBCFI.cpp
+++ llvm/lib/Target/X86/X86RSBCFI.cpp
@@ -1,7 +1,9 @@
 #include "X86.h"
 #include "X86InstrInfo.h"
+#include "X86Subtarget.h"
 #include "llvm/CodeGen/MachineFunctionPass.h"
 #include "llvm/CodeGen/MachineInstrBuilder.h"
+#include "llvm/Target/TargetMachine.h"
 
 using namespace llvm;
 
@@ -21,17 +23,48 @@ public:
 char X86RSBCFI::ID = 0;
 
 bool X86RSBCFI::runOnMachineFunction(MachineFunction &MF) {
+  const TargetMachine *TM = &MF.getTarget();
+  if (TM->getTargetTriple().getArch() != Triple::x86_64)
+    return false;
+
+  const X86Subtarget *STI = &MF.getSubtarget<X86Subtarget>();
+  const X86InstrInfo *TII = STI->getInstrInfo();
+
+#define RSBCFI_CALL_INSTR(MI)                                                  \
+  do {                                                                         \
+    BuildMI(MBB, MI, DebugLoc(), TII->get(X86::NOOP));                         \
+    BuildMI(MBB, MI, DebugLoc(), TII->get(X86::XTEST));                        \
+  } while (0);
+
+#define RSBCFI_RETL_INSTR(MI)                                                  \
+  do {                                                                         \
+    BuildMI(MBB, MI, DebugLoc(), TII->get(X86::NOOP));                         \
+    BuildMI(MBB, MI, DebugLoc(), TII->get(X86::NOOP));                         \
+  } while (0);
 
   for (auto &MBB : MF) {
-    outs() << "Contents of MachineBasicBlock:\n";
-    outs() << MBB << "\n";
-    const BasicBlock *BB = MBB.getBasicBlock();
-    outs() << "Contents of BasicBlock corresponding to MachineBasicBlock:\n";
-    outs() << *BB << "\n";
+    /* Do instrumentation after a CALL */
+    MachineInstr *preCall = NULL;
+    for (auto &MI : MBB) {
+      if (preCall != NULL)
+        RSBCFI_CALL_INSTR(preCall);
+
+      if (MI.isCall())
+        preCall = &MI;
+      else
+        preCall = NULL;
+    }
+    if (preCall != NULL)
+      RSBCFI_CALL_INSTR(preCall);
+
+    /* Do instrumentation before a RETURN */
+    MachineInstr &MI = MBB.back();
+    if (MI.isReturn())
+      RSBCFI_RETL_INSTR(MI);
   }
 
   return false;
-}
+} // namespace
 
 } // end of anonymous namespace
 
